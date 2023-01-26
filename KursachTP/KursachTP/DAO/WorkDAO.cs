1fa93665ca56fac2e5f4d527347e9085fabe85a7
RetroZoomer <bayan-herov@mail.ru>
2023-01-26 12:25:12 +0300

    Meh

--- KursachTP/KursachTP/DAO/WorkDAO.cs
+++ KursachTP/KursachTP/DAO/WorkDAO.cs
@@ -159,7 +159,7 @@ namespace KursachTP.DAO
             comanda.ExecuteNonQuery();
             Disconnect();
         }
-        public Warning GetWarning(int id_post, int id_us)
+        public void GetWarning(int id_post, int id_us)
         {
             Connect();
             string sql = "INSERT INTO Warning(id_post, id_user,warningdescription,warningtime) " +
@@ -177,7 +177,6 @@ namespace KursachTP.DAO
             comanda.ExecuteNonQuery(); //Срабатывает исключение
             Disconnect();
 
-            return null;
         }
 
         public void DeleteById(int id)
